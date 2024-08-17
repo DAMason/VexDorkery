810b044eba455ddb3a6b91233f800e276e439573
dmason <DAMason@gmail.com>
2024-08-17 11:06:56 -0500

    Catching up some apparent changes

--- NextInGit/NextTry/src/main.cpp
+++ NextInGit/NextTry/src/main.cpp
@@ -11,6 +11,23 @@
 
 using namespace vex;
 
+void MoveForward(float, int);
+
+
+
+bool FlyWheelSpinning=false;
+
+
+motor FrontLeftMotor = motor(PORT2, ratio18_1, false);
+motor FrontRightMotor = motor(PORT9, ratio18_1, true);
+motor BackLeftMotor = motor(PORT10, ratio18_1, false);
+motor BackRightMotor = motor(PORT1, ratio18_1, true);
+
+motor FlyWheelMotor = motor(PORT16,ratio6_1, true);
+
+controller Controller1 = controller(primary);
+
+
 // A global instance of competition
 competition Competition;
 
@@ -59,6 +76,9 @@ void autonomous(void) {
 /*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
+
+   FlyWheelMotor.setVelocity(0, rpm);
+
   // User control code here, inside the loop
   while (1) {
     // This is the main execution loop for the user control program.
@@ -68,11 +88,64 @@ void usercontrol(void) {
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
-    // ........................................................................
+    // ........................................................................    
+    FrontLeftMotor.setVelocity(Controller1.Axis3.position(), percent);
+    BackLeftMotor.setVelocity(Controller1.Axis3.position(), percent);
+    FrontRightMotor.setVelocity(Controller1.Axis2.position(), percent);
+    BackRightMotor.setVelocity(Controller1.Axis2.position(), percent);
+    FrontLeftMotor.spin(forward);
+    BackLeftMotor.spin(forward);
+    FrontRightMotor.spin(forward);
+    BackRightMotor.spin(forward);
+
+  // What is the state of button L1
+    bool L1Down = Controller1.ButtonL1.pressing();
+  // What is the state of button L1
+    bool L2Down = Controller1.ButtonL2.pressing();
+  // What is the state of button R1
+    bool R1Down = Controller1.ButtonR1.pressing();
+  // What is the state of button R1
+    bool R2Down = Controller1.ButtonR2.pressing();
+
+
+    if (R1Down) {
+      FlyWheelMotor.setVelocity(-500, rpm);
+    }
+    if (R2Down) {
+      FlyWheelMotor.setVelocity(0, rpm);
+    }
+
+    if (L1Down) {
+      FlyWheelMotor.setVelocity(500, rpm);
+    }
+    if (L2Down) {
+      FlyWheelMotor.setVelocity(0, rpm);
+    }
+
+    FlyWheelMotor.spin(forward);
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
+}
+
+
+
+// Function to move forward, howfar is in INCHES
+
+void MoveForward(float howfar, int howfast) {
+
+//units are wheel rotations
+
+
+  FrontLeftMotor.spinFor(forward,3,turns,false); //false keeps turning
+  FrontRightMotor.spinFor(forward,3,turns,false);
+  BackLeftMotor.spinFor(forward,3,turns,false);
+  BackRightMotor.spinFor(forward,3,turns,true); //so waits until this one ends
+
+ 
+return;
+
 }
 
 //
